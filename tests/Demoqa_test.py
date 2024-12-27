5bfcb96a196c08e5d26e3eb2bbcb0c39fcebf6f1
Michael Voronin <voroninma0912@gmail.com>
2024-12-27 16:36:58 +0500

    add: requirements gitignore

--- tests/Demoqa_test.py
+++ tests/Demoqa_test.py
@@ -33,3 +33,14 @@ def test_form():
     browser.element("#state").click().element(by.text("NCR")).click()
     browser.element("#city").click().element(by.text("Delhi")).click()
 
+    browser.element('[class="modal-header"').should(have.text('Thanks for submitting the form'))
+
+    browser.element('[class="table-responsive"]').should(have.text('Mike Voronin'))
+    browser.element('[class="table-responsive"]').should(have.text('test@mail.ru'))
+    browser.element('[class="table-responsive"]').should(have.text('Male'))
+    browser.element('[class="table-responsive"]').should(have.text('1234567891'))
+    browser.element('[class="table-responsive"]').should(have.text('12 September,1995'))
+    browser.element('[class="table-responsive"]').should(have.text('Sports'))
+    browser.element('[class="table-responsive"]').should(have.text('patriots.png'))
+    browser.element('[class="table-responsive"]').should(have.text('Ленина 1'))
+    browser.element('[class="table-responsive"]').should(have.text('NCR Delhi'))
